5503a8812bed4c8cd09edb36cae782faf7ac72a6
wonarii <ariellecwong@gmail.com>
2025-09-06 12:30:34 -0400

    Added Comments to the script
    -Modified the top-level comment to contain a description of the project.
    -Added function definition comments
    -Added Instruction-level comments

--- function-calls/js/script.js
+++ function-calls/js/script.js
@@ -1,27 +1,37 @@
 /**
- * Title of Project
- * Author Name
+ * The Duck
+ * Arielle Wong
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * A duck made of shapes is stuck in a vast field of yellow
+ * It is wondering why it itself isn't yellow too, and where
+ * it fits in in a world like this.
+ * 
+ * Uses:
+ * p5.js
+ * https://p5js.org/
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Creates a 640x480 canvas for the drawing to go on.
 */
 function setup() {
+    //Create the canvas at a standard resolution
     createCanvas(640, 480);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Draws a simple geometric duck on a yellow background
 */
 function draw() {
+    //A yellow Background
     background(255, 255, 100);
+    //The duck's beak
     rect(80, 80, 30, 30);
+    //The duck's head
     circle(150, 100, 100);
+    //The duck's body
     rect(100, 150, 200, 100);
 }
