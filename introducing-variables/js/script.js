37573d07cb4c61ecf07942c8e37261673b1eab12
wonarii <ariellecwong@gmail.com>
2025-09-08 15:21:52 -0400

    Fun Circles
    -Used variables to center circles
    -Used mouse position to control circle location, size and color

--- introducing-variables/js/script.js
+++ introducing-variables/js/script.js
@@ -8,17 +8,38 @@
 
 "use strict";
 
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
 function setup() {
-
+    // Create the canvas
+    createCanvas(480, 480);
 }
 
-
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
 function draw() {
+    background(0);
+    
+    // Draw a circle in the centre of the canvas
+    push();
+    noStroke();
+    fill(255, 255, 0);
+    //setting circle's coordinates to half the canvas size
+    //(dividing the dimensions by 2)
+    ellipse(width/2, height/2, 100, 100);
+    pop();
 
+
+    //moving circle
+    push();
+    noStroke();
+    fill(0, 255, 255);
+    //the circle will follow the mouse
+    ellipse(mouseX, mouseY, 100, 100);
+    pop();
+
+    //circle sizedepends on mouse
+    //also changes color
+    push();
+    noStroke();
+    fill(mouseX, mouseX-mouseY, mouseY);
+    //the circle will follow the mouse
+    ellipse(width/2, width/2, mouseX, mouseY);
+    pop();
 }
