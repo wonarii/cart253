c21bb60931e95dbc8de11c603cab19c89db33be9
wonarii <ariellecwong@gmail.com>
2025-09-10 15:46:34 -0400

    Found colours
    I tested different colours for the shapes and background and made sure they worked well together.
    It's important they are distinct enough to stack because I plan on using a strokeless style, so the shapes need to be easily distinguished.
    I also have them all in different push, pop statements, which could be helpful later when I start actually drawing.

--- art-jam/js/script.js
+++ art-jam/js/script.js
@@ -9,16 +9,62 @@
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Creates a square canvas
 */
 function setup() {
-
+    //640px x 640px canvas
+    createCanvas(640, 640);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Draws the self portrait
 */
 function draw() {
+    background("#ebd487ff");
+    //testing colors
+    //skin
+    push();
+    noStroke();
+    fill("#d8ab75ff");
+    circle(320,320,50);
+    pop();
+    //hair
+    push();
+    noStroke();
+    fill("#573a16ff");
+    circle(380,380,50);
+    pop();
+    //hair2
+    push();
+    noStroke();
+    fill("#f4be2bff");
+    circle(360,360,50);
+    pop();
+    //glasses frame
+    push();
+    noStroke();
+    fill("#100a01ff");
+    circle(420,380,50);
+    pop();
+    //fog
+    push();
+    noStroke();
+    fill("#e7e1d6ff");
+    circle(360,330,50);
+    pop();
 
+    //soup
+    push();
+    noStroke();
+    fill("#f39017ff");
+    circle(40,400,50);
+    pop();
+    //bowl
+    push();
+    noStroke();
+    fill("#6097a6ff");
+    circle(70,400,50);
+    pop();
+    
 }
