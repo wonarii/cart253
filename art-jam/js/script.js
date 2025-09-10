aa3adc3c8f38f3fe68874cbceb4a05271174b591
wonarii <ariellecwong@gmail.com>
2025-09-10 15:34:13 -0400

    Added template file for art jam project
    I added the template files for the art jam project.
    Gotta start somewhere.
    I modified the title and the main top comment (so I don't lose any points)

---
+++ art-jam/js/script.js
@@ -0,0 +1,24 @@
+/**
+ * Ari and Soup
+ * Arielle Wong
+ * 
+ * Self portrait
+ * 
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
