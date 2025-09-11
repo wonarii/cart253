0921cb17ab6a224526a702628f0e62e94a712169
wonarii <ariellecwong@gmail.com>
2025-09-11 13:35:19 -0400

    Used variables
    Rather than have numbers everywhere, we used variables to assign a value to a name.
    This makes the code easier to edit and read

--- creating-variables/js/script.js
+++ creating-variables/js/script.js
@@ -7,17 +7,37 @@
 
 "use strict";
 
+//cheese colour
+let cheeseRed = 255;
+let cheeseGreen = 215;
+let cheeseBlue = 40;
+
+//cheese hole
+let holeShade = 0; //greyscale value
+let holeX = 140;
+let holeY = 175;
+let holeSize = 170;
+
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Creates the canvas for the drawing
 */
 function setup() {
-
+    //create the canvas
+    createCanvas(480, 480);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Draws a hole in cheese
 */
 function draw() {
+    //cheese colour (yellow)
+    background(cheeseRed, cheeseGreen, cheeseBlue);
 
+    //draw a hole in upper left
+    push();
+    noStroke();
+    fill(holeShade);
+    ellipse(holeX,holeY,holeSize);
+    pop();
 }
