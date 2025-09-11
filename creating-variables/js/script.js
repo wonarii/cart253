2b5cf69e3f96ecb2b4c3e3b6dd188eab83d1d6cb
wonarii <ariellecwong@gmail.com>
2025-09-11 13:28:01 -0400

    Setting up creating variables tutorial
    -Downloaded the template
    -Changed the title
    -Updated the top comment in the script

---
+++ creating-variables/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Creating variables
+ * Arielle Wong
+ * 
+ * Learning how to create variables!
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
