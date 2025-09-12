309369f4f258542bf49e08c11c9a6f0e2e6341d3
wonarii <ariellecwong@gmail.com>
2025-09-12 19:45:58 -0400

    Added the template for movement variables
    Downloaded the template, renamed it and then added a title and top comment in the script

---
+++ 02-Variables/movement-variables/js/script.js
@@ -0,0 +1,24 @@
+/**
+ * Movement variables
+ * 
+ * Author: Arielle Wong and Pippin
+ * 
+ * p5.js
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
