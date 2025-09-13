32a8d9156d6b1543ddfb8bd591354fcbb889d7c9
wonarii <ariellecwong@gmail.com>
2025-09-13 10:36:37 -0400

    Made a bird move more realistically
    By adding variables for both velocity and acceleration that interact with each other, the bird moves in a more realistic way.
    It will slowly accelerate to its max speed rather than immediately moving at max capacity.

--- 02-Variables/movement-variables/js/script.js
+++ 02-Variables/movement-variables/js/script.js
@@ -8,17 +8,59 @@
 
 "use strict";
 
+//bird variables
+let bird = {
+    x:120,
+    y:480,
+    size:50,
+    //velocity
+    velocity: {
+        x: 0,
+        y: 0,
+    },
+    minVelocity: {
+      //minimum speed of bird (can go backwards)
+      x: -3,
+      y: -2  
+    },
+    // maximum velocity (not too fast)
+    maxVelocity: {
+        x: 3,
+        y: 2
+    },
+    //will be added to the velocity every frame
+    acceleration:{
+        x:0.025,
+        y:-0.05
+    }
+};
+
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Creates a canvas
 */
 function setup() {
-
+    createCanvas(640, 640);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Draws a bird moving across a black background
 */
 function draw() {
+    background(0);
 
+    //Change the bird's velocity by adding the acceleration
+    bird.velocity.x = bird.velocity.x + bird.acceleration.x;
+    bird.velocity.y = bird.velocity.y + bird.acceleration.y;
+   
+    //constrain the bird's velocity
+    bird.velocity.x = constrain(bird.velocity.x, bird.minVelocity.x, bird.maxVelocity.x);
+    bird.velocity.y = constrain(bird.velocity.y, bird.minVelocity.y, bird.maxVelocity.y);
+    
+    //Move the bird
+    bird.x = bird.x+bird.velocity.x;
+    bird.y = bird.y+ bird.velocity.y;
+
+    //Draw the bird
+    ellipse(bird.x,bird.y, bird.size);
 }
