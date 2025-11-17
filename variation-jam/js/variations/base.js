20521d69d3a43e9e4ba82afbf4e49205fc423fd3
wonarii <ariellecwong@gmail.com>
2025-11-17 15:37:31 -0500

    There's a question mark
    A yellow question mark on a dark background.
    
    Starting off simple.
    
    Prepared some of my fonts too. I got them from Google fonts.
    
    Next up will be to add a text box to collect input

--- variation-jam/js/variations/base.js
+++ variation-jam/js/variations/base.js
@@ -3,6 +3,29 @@
  * Vanilla version if you will.
  */
 
+
+const fonts = {
+    bagel: undefined,
+    montserrat: undefined,
+}
+
+const colours = {
+    backgroundColour : "#36243eff",
+    yellow: "#fdca32ff",
+}
+
+//preload
+//runs before anything else
+//loads my fonts so they're ready to use!
+function preload(){
+    fonts.bagel = loadFont("./assets/BagelFatOne-Regular.ttf");
+    fonts.montserrat = loadFont("./assets/Montserrat-VariableFont_wght.ttf");
+}
+
+
+
+
+
 /**
  * This will be called just before the red variation starts
  */
@@ -11,10 +34,15 @@ function baseSetup() {
 }
 
 /**
- * This will be called every frame when the red variation is active
+ * This will be called every frame when the base variation is called
  */
 function baseDraw() {
-    background("red");
+    background(colours.backgroundColour);
+    drawQuestionMark();
+    // drawInputField();
+    // drawGuessButton();
+    // drawGuesses();
+
 }
 
 /**
@@ -30,5 +58,17 @@ function baseKeyPressed(event) {
  * This will be called whenever the mouse is pressed while the red variation is active
  */
 function baseMousePressed() {
+
+}
+
+function drawQuestionMark(){
+    push();
+    textFont(fonts.bagel);
+    textAlign(CENTER, CENTER);
+    fill(colours.yellow);
+    textSize(72);
+    text("?", width/2,height/4);
+
+    pop();
 
 }
